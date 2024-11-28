76b44547f3fc33d2d4ace24843e4448b788c717e
Diony Rosa <dhrosa@gmail.com>
2024-11-28 15:25:49 -0500

    Game creation API

--- be/src/be/main.py
+++ be/src/be/main.py
@@ -1,24 +1,69 @@
 from fastapi import FastAPI
 from fastapi_camelcase import CamelModel
 import coolname
+from pydantic import Field
+from typing import Literal
 
-class Lobby(CamelModel):
+
+class GameSettings(CamelModel):
+    player_count: int
+    player_word_length: int
+
+
+class PendingGame(CamelModel):
+    phase: Literal["pending"] = "pending"
+
+    seats: list[str]
+
+
+class Game(CamelModel):
     id: str
+    settings: GameSettings
+    state: PendingGame
 
-app = FastAPI()
 
-lobbies = dict[str, Lobby]
+class NewGameRequest(CamelModel):
+    player_count: int
+    player_word_length: int
+
+
+class ClaimSeatRequest(CamelModel):
+    seat_index: int
+    player_name: str
+
+
+app = FastAPI(root_url="/api")
+
 
 @app.get("/player_name")
-async def player_name():
+async def player_name() -> str:
     return " ".join(x.capitalize() for x in coolname.generate(2))
 
-@app.post("/lobby/new")
-async def lobby_new() -> Lobby:
-    return Lobby(id=coolname.generate_slug(3))
 
-@app.post("/lobby/join/{lobby_id}")
-async def lobby_id(lobby_id: str):
-    lobby = lobbies.get(lobby_id)
-    if lobby is None:
-        raise HTTPException(status_code=404, detail="Lobby not found")
+games = dict[str, Game]()
+
+
+@app.get("/game")
+async def game_list() -> list[str]:
+    return list(games.keys())
+
+
+@app.get("/game/{game_id}")
+async def game_get(game_id: str) -> Game:
+    game = games.get(game_id)
+    if game is None:
+        raise HTTPException(status_code=404, detail="Game not found")
+    return game
+
+
+@app.post("/game")
+async def game_new(settings: GameSettings) -> Game:
+    game = Game(
+        id=coolname.generate_slug(2),
+        settings=settings,
+        state=PendingGame(
+            seats=[""] * settings.player_count,
+        ),
+    )
+    games[game.id] = game
+    return game
