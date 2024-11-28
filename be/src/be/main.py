eeedd33ba099f3fe338d2e316fe00742d8cc0a77
Diony Rosa <dhrosa@gmail.com>
2024-11-27 22:19:54 -0500

    Play around with slug generation.

--- be/src/be/main.py
+++ be/src/be/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi_camelcase import CamelModel
+import coolname
+
+class Lobby(CamelModel):
+    id: str
 
 app = FastAPI()
 
+lobbies = dict[str, Lobby]
 
-@app.get("/")
-def hello():
-    return {"message": "sup"}
+@app.get("/player_name")
+async def player_name():
+    return " ".join(x.capitalize() for x in coolname.generate(2))
+
+@app.post("/lobby/new")
+async def lobby_new() -> Lobby:
+    return Lobby(id=coolname.generate_slug(3))
+
+@app.post("/lobby/join/{lobby_id}")
+async def lobby_id(lobby_id: str):
+    lobby = lobbies.get(lobby_id)
+    if lobby is None:
+        raise HTTPException(status_code=404, detail="Lobby not found")
