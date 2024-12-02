f2f2296ac386b2982622045c22e08293d7e47aa3
Diony Rosa <dhrosa@gmail.com>
2024-12-02 01:07:37 -0500

    Start websocket page logic

--- fe/GamePage.tsx
+++ fe/GamePage.tsx
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
 import React from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 export default function GamePage() {
   const { gameId } = useParams<{ gameId: string }>();
   const [gameData, setGameData] = React.useState<object | null>(null);
+
+  const { lastJsonMessage, readyState } = useWebSocket(
+    `/api/game/${gameId ?? "unknown"}/player/A`
+  );
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
   React.useEffect(() => {
     (async () => {
       const response = await fetch(`/api/game/${gameId ?? "unknown"}`);
@@ -17,13 +30,19 @@ export default function GamePage() {
       console.error(error);
     });
   }, []);
-
+  console.log("Message:", lastJsonMessage);
+  console.log("Connection status: ", connectionStatus);
   if (gameData === null) {
     return <p>Loading...</p>;
   }
   return (
     <>
-      <section className="section">
+      <section className="section container">
+        <p>
+          Socket status: <pre>{connectionStatus}</pre>
+        </p>
+        <h3>WebSocket message</h3>
+        <pre>{JSON.stringify(lastJsonMessage, null, 2)}</pre>
         <h3>Raw Data</h3>
         <pre>{JSON.stringify(gameData, null, 2)}</pre>
       </section>
