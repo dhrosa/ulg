cf6745152cd38eedc34cf2b5f78f58ee3b288049
Diony Rosa <dhrosa@gmail.com>
2024-12-25 20:16:14 -0500

    Add theme selector

--- fe/Nav.tsx
+++ fe/Nav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Symbol from "./Symbol";
 
 function NavLink({ href, label }: { href: string; label: string }) {
   return (
@@ -32,6 +33,39 @@ function HamburgerButton({
   );
 }
 
+function ThemeDropDown() {
+  const setTheme = (theme: string) => {
+    document.documentElement.dataset["theme"] = theme;
+  };
+  return (
+    <div className="dropdown is-hoverable">
+      <div className="dropdown-trigger">
+        <button className="button">
+          <span>Theme</span>
+          <span className="icon is-small">
+            <Symbol name="arrow_drop_down" />
+          </span>
+        </button>
+      </div>
+      <div className="dropdown-menu">
+        <div className="dropdown-content">
+          {["Light", "Dark"].map((theme) => (
+            <a
+              key={theme}
+              className="dropdown-item"
+              onClick={() => {
+                setTheme(theme.toLowerCase());
+              }}
+            >
+              {theme}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Nav() {
   const [menuActive, setMenuActive] = React.useState(false);
 
@@ -51,6 +85,9 @@ export default function Nav() {
           <NavLink href="/" label="Home" />
           <NavLink href="/new" label="New Game" />
           <NavLink href="/words" label="Words" />
+          <div className="navbar-item">
+            <ThemeDropDown />
+          </div>
         </div>
       </div>
     </nav>
